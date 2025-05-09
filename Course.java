663a67dd7c3c675163a9e5ca229e763977c75aac
Heshan925 <malinduheshan925@gmail.com>
2025-05-09 19:45:33 +0530

    final submission

---
+++ Course.java
@@ -0,0 +1,10 @@
+package com.university.model;
+
+public class Course {
+    private int courseId;
+    private String name;
+    private String instructor;
+    private int credits;
+
+    // Getters and Setters
+}
